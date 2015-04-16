4d2bcd6b5941c378b4b15ff26f67c59df2226fcf
deadprogram <ron@hybridgroup.com>
2015-04-15 20:37:51 -0700

    Add dependencies and test stubs green

--- spec/lib/index.spec.js
+++ spec/lib/index.spec.js
@@ -8,19 +8,19 @@ var Adaptor = source("adaptor"),
 describe("index", function() {
   describe("#adaptors", function() {
     it("is an array of supplied adaptors", function() {
-      expect(index.adaptors).to.be.eql([]);
+      expect(index.adaptors).to.be.eql(["bean"]);
     });
   });
 
   describe("#drivers", function() {
     it("is an array of supplied drivers", function() {
-      expect(index.drivers).to.be.eql([]);
+      expect(index.drivers).to.be.eql(["bean"]);
     });
   });
 
   describe("#dependencies", function() {
     it("is an array of supplied dependencies", function() {
-      expect(index.dependencies).to.be.eql([]);
+      expect(index.dependencies).to.be.eql(["cylon-gpio"]);
     });
   });
 
